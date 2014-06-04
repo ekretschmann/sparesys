23d330f7cbce4188ee0566c3e1cadb3c6efde142
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-06-04 13:26:10 +0100

    crud module courses added

---
+++ app/controllers/courses.server.controller.js
@@ -0,0 +1,130 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Course = mongoose.model('Course'),
+	_ = require('lodash');
+
+/**
+ * Get the error message from error object
+ */
+var getErrorMessage = function(err) {
+	var message = '';
+
+	if (err.code) {
+		switch (err.code) {
+			case 11000:
+			case 11001:
+				message = 'Course already exists';
+				break;
+			default:
+				message = 'Something went wrong';
+		}
+	} else {
+		for (var errName in err.errors) {
+			if (err.errors[errName].message) message = err.errors[errName].message;
+		}
+	}
+
+	return message;
+};
+
+/**
+ * Create a Course
+ */
+exports.create = function(req, res) {
+	var course = new Course(req.body);
+	course.user = req.user;
+
+	course.save(function(err) {
+		if (err) {
+			return res.send(400, {
+				message: getErrorMessage(err)
+			});
+		} else {
+			res.jsonp(course);
+		}
+	});
+};
+
+/**
+ * Show the current Course
+ */
+exports.read = function(req, res) {
+	res.jsonp(req.course);
+};
+
+/**
+ * Update a Course
+ */
+exports.update = function(req, res) {
+	var course = req.course;
+
+	course = _.extend(course, req.body);
+
+	course.save(function(err) {
+		if (err) {
+			return res.send(400, {
+				message: getErrorMessage(err)
+			});
+		} else {
+			res.jsonp(course);
+		}
+	});
+};
+
+/**
+ * Delete an Course
+ */
+exports.delete = function(req, res) {
+	var course = req.course;
+
+	course.remove(function(err) {
+		if (err) {
+			return res.send(400, {
+				message: getErrorMessage(err)
+			});
+		} else {
+			res.jsonp(course);
+		}
+	});
+};
+
+/**
+ * List of Courses
+ */
+exports.list = function(req, res) {
+	Course.find().sort('-created').populate('user', 'displayName').exec(function(err, courses) {
+		if (err) {
+			return res.send(400, {
+				message: getErrorMessage(err)
+			});
+		} else {
+			res.jsonp(courses);
+		}
+	});
+};
+
+/**
+ * Course middleware
+ */
+exports.courseByID = function(req, res, next, id) {
+	Course.findById(id).populate('user', 'displayName').exec(function(err, course) {
+		if (err) return next(err);
+		if (!course) return next(new Error('Failed to load Course ' + id));
+		req.course = course;
+		next();
+	});
+};
+
+/**
+ * Course authorization middleware
+ */
+exports.hasAuthorization = function(req, res, next) {
+	if (req.course.user.id !== req.user.id) {
+		return res.send(403, 'User is not authorized');
+	}
+	next();
+};
---
+++ app/routes/courses.server.routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+	var users = require('../../app/controllers/users');
+	var courses = require('../../app/controllers/courses');
+
+	// Courses Routes
+	app.route('/courses')
+		.get(courses.list)
+		.post(users.requiresLogin, courses.create);
+	
+	app.route('/courses/:courseId')
+		.get(courses.read)
+		.put(users.requiresLogin, courses.hasAuthorization, courses.update)
+	    .delete(users.requiresLogin, courses.hasAuthorization, courses.delete);
+
+	// Finish by binding the Course middleware
+	app.param('courseId', courses.courseByID);
+};

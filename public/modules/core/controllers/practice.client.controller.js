4c61b60694826a4be85c1e9840f54bdfbcb5d0ac
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-06-21 11:11:18 +0100

    async loadCards works with promise

--- public/modules/core/controllers/practice.client.controller.js
+++ public/modules/core/controllers/practice.client.controller.js
@@ -3,48 +3,79 @@
 
 // Courses controller
 angular.module('core').controller('PracticeController',
-    ['$scope', '$stateParams', '$location', '$modal', '$timeout','Authentication', 'Courses', 'Packs', 'Cards',
-        function ($scope, $stateParams, $location, $modal, $timeout, Authentication, Courses, Packs, Cards) {
+    ['$scope', '$q', '$stateParams', '$state', '$location', '$modal', '$timeout', '$document', 'Authentication', 'Courses', 'Packs', 'Cards',
+        function ($scope, $q, $stateParams, $state, $location, $modal, $timeout, $document, Authentication, Courses, Packs, Cards) {
             $scope.authentication = Authentication;
 
 
-            var cardsLoaded = false;
+            $scope.showAnswer = false;
             $scope.cards = [];
+            $scope.card = undefined;
 
-            // Find existing Course
-            $scope.findOne = function () {
+            $document.bind('keypress', function (event) {
+                if (event.keyCode === 13) {
+                    $scope.toggleAnswer();
+                }
+            });
 
+
+            $scope.loadCards = function (callback) {
+                var deferred = $q.defer();
                 $scope.course = Courses.get({
                     courseId: $stateParams.courseId
-                }, function(course) {
-                    course.packs.forEach(function(packId) {
+                }, function (course) {
+                    course.packs.forEach(function (packId) {
                         var packs = Packs.get({
                             packId: packId
-                        }, function(pack) {
-                            pack.cards.forEach(function(cardId) {
-                               Cards.get({
-                                   cardId: cardId
-                               }, function(card) {
-                                   $scope.cards.push(card);
-                               });
+                        }, function (pack) {
+                            pack.cards.forEach(function (cardId) {
+                                Cards.get({
+                                    cardId: cardId
+                                }, function (card) {
+                                    $scope.cards.push(card);
+                                    deferred.resolve();
+                                });
                             });
-                            console.log('done')
-                            console.log($scope.cards)
 
                         });
                     });
                 });
+
+                return deferred.promise;
+            };
+            // Find existing Course
+            $scope.init = function () {
+                $scope.loadCards().then(function () {
+
+                    $scope.card = $scope.cards[0];
+                    $state.go($state.current);
+                });
+
+
             };
 
-            $scope.nextCard = function() {
-//                while(!cardsLoaded) {
-//                    $timeout(function(){
-////                    angular.element('.addpackfocus').trigger('focus');
-//
-//                    },1000);
-//                }
 
-                console.log($scope.cards);
+            $scope.index = 0;
+            $scope.nextCard = function () {
+                $scope.index = $scope.index + 1;
+                if ($scope.cards.length <= $scope.index) {
+                    $scope.index = 0;
+                }
+
+                return $scope.cards[$scope.index];
+
             };
+
+            $scope.toggleAnswer = function () {
+
+                if ($scope.showAnswer) {
+                    $scope.card = $scope.nextCard();
+                }
+                $scope.showAnswer = !$scope.showAnswer;
+                $state.go($state.current);
+            };
+
+
         }
-    ]);
+    ])
+;

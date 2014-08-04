13860198ea3963685d90234eb7c6dc1e87d34406
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-08-04 14:44:13 +0100

    students cant modify school courses. Temporary.

--- app/models/course.server.model.js
+++ app/models/course.server.model.js
@@ -43,7 +43,7 @@ var CourseSchema = new Schema({
         default: ''
     },
     master: {
-        type: [Schema.Types.ObjectId]
+        type: Schema.Types.ObjectId
     },
     visible: {
         type: Boolean,

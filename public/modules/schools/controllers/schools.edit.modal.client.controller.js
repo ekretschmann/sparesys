bcf0366dffea5d1554ccc7c911a6f2651fbdd9b8
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-08-07 18:28:10 +0100

    proceeding with school management

--- public/modules/schools/controllers/schools.edit.modal.client.controller.js
+++ public/modules/schools/controllers/schools.edit.modal.client.controller.js
@@ -4,10 +4,11 @@ angular.module('schools').controller('EditSchoolController', ['$scope', '$state'
     function ($scope, $state, $timeout, $modalInstance, school) {
         $scope.school = school;
 
+        $scope.model = {};
 
         $scope.setFocus = function () {
             $timeout(function () {
-                angular.element('.editPackFocus').trigger('focus');
+                angular.element('.focus').trigger('focus');
             }, 100);
         };
 
@@ -17,12 +18,11 @@ angular.module('schools').controller('EditSchoolController', ['$scope', '$state'
 
         $scope.ok = function () {
 
-
+            $scope.school.name = $scope.model.name;
+            $scope.school.city = $scope.model.city;
+            $scope.school.country = $scope.model.country;
 
             $scope.school.$update();
-
-
-
 
             $modalInstance.dismiss('cancel');
         };

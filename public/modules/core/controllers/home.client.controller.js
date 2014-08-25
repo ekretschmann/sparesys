8268031ba337ddfc9c021fd2b50750ac3b47835f
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-08-25 11:51:26 +0100

    getting slide show working

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -8,17 +8,19 @@ angular.module('core').controller('HomeController', ['$scope', '$modal', 'Authen
 
         // Set of Photos
         $scope.slides = [
-            {src: '/modules/core/img/brand/superhero-girl-medium.gif', text: 'test'},
-            {src: '/modules/core/img/brand/superhero-boy-medium.gif', text: 'Image 02'},
-            {src: '/modules/core/img/brand/philosopher-medium.gif', text: 'Image 03'},
-            {src: '/modules/core/img/brand/guru-medium.gif', text: 'Image 04'},
-            {src: '/modules/core/img/brand/teacher-man-medium.gif', text: 'Image 05'},
-            {src: '/modules/core/img/brand/teacher-woman-medium.gif', text: 'Image 06'}
+            {image: '/modules/core/img/brand/superhero-girl-medium.gif', text: 'You won\'t forget this. Ever.'},
+            {image: '/modules/core/img/brand/superhero-boy-medium.gif', text: 'I dare you to become a Rememberator!'},
+            {image: '/modules/core/img/brand/philosopher-medium.gif', text: 'I remember, therefore I am.'},
+            {image: '/modules/core/img/brand/guru-medium.gif', text: 'Unleash your brainpower!'},
+            {image: '/modules/core/img/brand/teacher-man-medium.gif', text: 'That will teach you!'},
+            {image: '/modules/core/img/brand/teacher-woman-medium.gif', text: 'Forget about Forgetting!'}
         ];
+
+
 
         // initial image index
 //        $scope.index = Math.floor((Math.random() * 6) + 1);
-        $scope.index = 2;
+//        $scope.index = 2;
 
 
         $scope.signinPopup = function () {
---
+++ public/modules/core/directives/carousel.client.directive.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('core').directive('imagecarousel',
+	function($timeout) {
+        return {
+            restrict: 'AE',
+            replace: true,
+            scope: {
+                slides: '='
+
+
+            },
+            link: function(scope, elem, attrs) {
+                scope.currentIndex = 0; // Initially the index is at the first image
+
+                scope.next = function() {
+                    if (scope.currentIndex < scope.slides.length - 1) {
+                        scope.currentIndex++;
+                    } else {
+                        scope.currentIndex = 0;
+                    }
+                };
+
+                scope.prev = function() {
+                    if (scope.currentIndex > 0) {
+                        scope.currentIndex--;
+                    } else {
+                        scope.currentIndex = scope.slides.length - 1;
+                    }
+                };
+
+                scope.$watch('currentIndex', function() {
+                    scope.slides.forEach(function(slide) {
+                        slide.visible = false; // make every image invisible
+                    });
+
+                    scope.slides[scope.currentIndex].visible = true; // make the current image visible
+                });
+
+
+                /* Start: For Automatic slideshow*/
+
+                var timer;
+
+                var sliderFunc=function(){
+                    timer=$timeout(function(){
+                        scope.next();
+                        timer=$timeout(sliderFunc,500);
+                    },5000);
+                };
+
+                sliderFunc();
+
+                scope.$on('$destroy',function(){
+                    $timeout.cancel(timer);
+                });
+
+                /* End : For Automatic slideshow*/
+                
+            },
+            templateUrl: '/modules/core/views/templates/carousel.html'
+        };
+	}
+);
+
+

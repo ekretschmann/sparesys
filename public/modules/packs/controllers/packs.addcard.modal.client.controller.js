6896de1f3406f9bec79bbb01b37837095f66053a
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-09-03 15:19:42 +0100

    adding cards now synchronized

--- public/modules/packs/controllers/packs.addcard.modal.client.controller.js
+++ public/modules/packs/controllers/packs.addcard.modal.client.controller.js
@@ -18,52 +18,89 @@ angular.module('packs').controller('AddCardToPackController', ['$scope', '$state
 
         $scope.addCardToPack = function () {
 
-            var packs = [];
+
             var original = new Cards({
                 question: this.question,
                 answer: this.answer,
                 packs: [$scope.pack._id],
                 slaves: []
             });
+            var self = {};
+            self.question = this.question;
+            self.answer = this.answer;
 
-            if (pack.slaves) {
-                pack.slaves.forEach(function (slaveId) {
-                    packs.push(slaveId);
-                }, this);
-            }
-
-            packs.forEach(function (pId) {
-
-                var self = {};
-                self.question = this.question;
-                self.answer = this.answer;
-
-                Packs.query({
-                    _id: pId
-                }, function (packs) {
-                    var card = new Cards({
-                        question: self.question,
-                        answer: self.answer,
-                        packs: [pId],
-                        user: packs[0].user
-                    });
-
-                    card.$save();
-                    console.log(original);
-                    original.slaves.push(card._id);
-
-
-                    packs[0].cards.push(card._id);
-                    packs[0].$update();
+            original.$save(function () {
+                $scope.pack.cards.push(original._id);
+                $scope.pack.$update(function () {
+                    $state.go($state.$current, null, { reload: true });
                 });
-            }, this);
 
-            console.log('here');
-            console.log(original);
 
-            original.$save(function() {
-                $state.go($state.$current, null, { reload: true });
+                self.slaves = [];
+                self.slavesToSave = pack.slaves.length;
+                self.slavesSaved = 0;
+                // take care of slaves
+                pack.slaves.forEach(function (slaveId) {
+
+                    Packs.query({
+                        _id: slaveId
+                    }, function (slavePacks) {
+                        if (slavePacks.length === 1) {
+                            var slave = slavePacks[0];
+                            var card = new Cards({
+                                question: self.question,
+                                answer: self.answer,
+                                packs: [slaveId]
+                            });
+                            card.$save(function () {
+                                slave.cards.push(card._id);
+                                slave.$update();
+                                self.slaves.push(card._id);
+                                self.slavesSaved ++;
+                                if(self.slavesSaved === self.slavesToSave) {
+                                    original.slaves = self.slaves;
+                                    original.$update();
+                                }
+                            });
+
+                        }
+                    });
+                }, this);
+
             });
+
+
+//            packs.forEach(function (pId) {
+//
+//                var self = {};
+//                self.question = this.question;
+//                self.answer = this.answer;
+//
+//                Packs.query({
+//                    _id: pId
+//                }, function (packs) {
+//                    var card = new Cards({
+//                        question: self.question,
+//                        answer: self.answer,
+//                        packs: [pId],
+//                        user: packs[0].user
+//                    });
+//
+//                    card.$save();
+//                    original.slaves.push(card._id);
+//                    console.log('here');
+//                    console.log(original);
+//
+//                    original.$save(function() {
+//                        $state.go($state.$current, null, { reload: true });
+//                    });
+//
+//
+//                    packs[0].cards.push(card._id);
+//                    packs[0].$update();
+//                });
+//            }, this);
+
 
             JourneyService.cardCreated();
             this.question = '';

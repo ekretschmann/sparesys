606b13bab850965eaa27558f541c724389c0610e
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-09-03 16:36:06 +0100

    adding packs now synchronized

--- public/modules/courses/controllers/courses.addpack.modal.client.controller.js
+++ public/modules/courses/controllers/courses.addpack.modal.client.controller.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('courses').controller('AddPackToCourseController', ['$scope', '$state', '$timeout', '$modalInstance', 'course', 'Packs', 'JourneyService',
-	function($scope, $state, $timeout, $modalInstance, course, Packs, JourneyService) {
+angular.module('courses').controller('AddPackToCourseController', ['$scope', '$state', '$timeout', '$modalInstance', 'course', 'Packs', 'Courses','JourneyService',
+	function($scope, $state, $timeout, $modalInstance, course, Packs, Courses, JourneyService) {
         $scope.course = course;
 
         $scope.setFocus = function () {
             $timeout(function(){
-                angular.element('.addpackfocus').trigger('focus');
+                angular.element('.focus').trigger('focus');
             },100);
         };
 
@@ -15,6 +15,12 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
         };
 
         $scope.addPackToCourse = function () {
+
+            var self = {};
+            self.name = this.name;
+            self.slavesToSave = course.slaves.length;
+            self.slavesSaved = 0;
+            self.slaves = [];
 
             // Create new Pack object
             var pack = new Packs({
@@ -26,22 +32,53 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
             // Redirect after save
             pack.$save(function (response) {
                 var packid = response._id;
-                var c = $scope.course;
-                c.packs.push(packid);
-                c.$update(function () {
+                $scope.course.packs.push(packid);
+                $scope.course.$update(function () {
                     $scope.name = '';
-                    JourneyService.packCreated();
                     $state.go($state.$current, null, { reload: true });
                 }, function (errorResponse) {
                     $scope.error = errorResponse.data.message;
                 });
 
 
+                $scope.course.slaves.forEach(function(slaveId) {
+                    console.log(slaveId);
+                    Courses.query({
+                        _id: slaveId
+                    }, function (slaveCourses) {
+
+                        if (slaveCourses.length === 1) {
+                            var slaveCourse = slaveCourses[0];
+
+                            var slavePack = new Packs({
+                                name: self.name,
+                                course: slaveCourse._id
+                            });
+
+                            slavePack.$save(function() {
+                                slaveCourse.packs.push(slavePack._id);
+                                self.slaves.push(slavePack._id);
+                                self.slavesSaved ++;
+                                if (self.slavesSaved === self.slavesToSave) {
+                                    pack.slaves = self.slaves;
+                                    pack.$update();
+                                    slaveCourse.$update();
+                                }
+                            });
+
+
+                        }
+                    });
+                });
+
+
             }, function (errorResponse) {
                 $scope.error = errorResponse.data.message;
             });
+
+            JourneyService.packCreated();
             this.name = '';
-            angular.element('.addpackfocus').trigger('focus');
+            angular.element('.focus').trigger('focus');
         };
 	}
 ]);

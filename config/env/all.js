f3cebe815d43e2a47e650be85893dc9c6caecf5f
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-09-04 09:44:47 +0100

    trying to fix production

--- config/env/all.js
+++ config/env/all.js
@@ -18,7 +18,7 @@ module.exports = {
 			],
 			js: [
                 'public/lib/jquery/dist/jquery.js',
-                'public/lib/jquery-ui/ui/jquery-ui.js',
+                'public/lib/jquery-ui/jquery-ui.js',
 				'public/lib/angular/angular.js',
 				'public/lib/q/q.js',
 				'public/lib/angular-resource/angular-resource.js', 

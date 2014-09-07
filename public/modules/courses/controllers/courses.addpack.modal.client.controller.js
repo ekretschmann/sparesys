4394b373a403b7c57dffb4455ca4a0695d71a4bb
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-09-07 11:07:30 +0100

    removed debug info

--- public/modules/courses/controllers/courses.addpack.modal.client.controller.js
+++ public/modules/courses/controllers/courses.addpack.modal.client.controller.js
@@ -31,7 +31,6 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
 
             // Redirect after save
             pack.$save(function (response) {
-                console.log('saved pack');
                 var packid = response._id;
                 $scope.course.packs.push(packid);
                 $scope.course.$update(function () {
@@ -44,14 +43,12 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
                 console.log($scope.course.slaves);
 
                 $scope.course.slaves.forEach(function(slaveId) {
-                    console.log('got slave: '+slaveId);
                     Courses.query({
                         _id: slaveId
                     }, function (slaveCourses) {
 
                         if (slaveCourses.length === 0) {
                             // course does not exist any more
-                            console.log('deleting course '+slaveId);
                             self.slavesSaved ++;
                             for (var i in $scope.course.slaves) {
                                 if ($scope.course.slaves[i] === slaveId) {
@@ -61,7 +58,6 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
                         }
 
                         if (slaveCourses.length === 1) {
-                            console.log('creating slave pack');
                             var slaveCourse = slaveCourses[0];
 
                             var slavePack = new Packs({
@@ -70,12 +66,10 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
                             });
 
                             slavePack.$save(function() {
-                                console.log('saving pack');
                                 slaveCourse.packs.push(slavePack._id);
                                 self.slaves.push(slavePack._id);
                                 self.slavesSaved ++;
                                 if (self.slavesSaved === self.slavesToSave) {
-                                    console.log('updating');
                                     pack.slaves = self.slaves;
                                     pack.$update();
                                     slaveCourse.$update();

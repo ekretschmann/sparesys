36f39e742de74d4739e87c9030a6d64032c65813
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-09-17 07:23:09 +0100

    cleaning up message handling

--- public/modules/messages/config/messages.client.routes.js
+++ public/modules/messages/config/messages.client.routes.js
@@ -2,24 +2,30 @@
 
 //Setting up route
 angular.module('messages').config(['$stateProvider',
-	function($stateProvider) {
-		// Messages state routing
-		$stateProvider.
-		state('listMessages', {
-			url: '/messages',
-			templateUrl: 'modules/messages/views/list-messages.client.view.html'
-		}).
-		state('createMessage', {
-			url: '/messages/create',
-			templateUrl: 'modules/messages/views/create-message.client.view.html'
-		}).
-		state('viewMessage', {
-			url: '/messages/:messageId',
-			templateUrl: 'modules/messages/views/view-message.client.view.html'
-		}).
-		state('editMessage', {
-			url: '/messages/:messageId/edit',
-			templateUrl: 'modules/messages/views/edit-message.client.view.html'
-		});
-	}
+    function ($stateProvider) {
+        // Messages state routing
+        $stateProvider.
+            state('adminMessages', {
+                url: '/messages/admin',
+                templateUrl: 'modules/messages/views/admin-messages.client.view.html'
+            })
+//            .
+//            state('listMessages', {
+//                url: '/messages',
+//                templateUrl: 'modules/messages/views/list-messages.client.view.html'
+//            }).
+//            state('createMessage', {
+//                url: '/messages/create',
+//                templateUrl: 'modules/messages/views/create-message.client.view.html'
+//            }).
+//            state('viewMessage', {
+//                url: '/messages/:messageId',
+//                templateUrl: 'modules/messages/views/view-message.client.view.html'
+//            }).
+//            state('editMessage', {
+//                url: '/messages/:messageId/edit',
+//                templateUrl: 'modules/messages/views/edit-message.client.view.html'
+//            })
+        ;
+    }
 ]);

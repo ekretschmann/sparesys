c5a5a76d4604f0b46c92627c839d00a412fcbfb7
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-09-25 19:13:29 +0100

    infobox directive advancing

--- public/modules/core/directives/infobox.client.directive.js
+++ public/modules/core/directives/infobox.client.directive.js
@@ -3,16 +3,22 @@
 angular.module('core').directive('infobox',
 	function($timeout) {
         return {
-            restrict: 'E',
+            restrict: 'AE',
             transclude: true,
             scope: {
-                character: '='
+                logo: '@',
+                title: '@',
+                position: '@'
             },
             link: function(scope, elem, attrs) {
-                console.log('here');
-                console.log(elem);
-                console.log(attrs);
-
+                switch (scope.logo) {
+                    case 'woman': scope.image = '/modules/core/img/brand/teacher-woman-medium.gif'; break;
+                    case 'girl': scope.image = '/modules/core/img/brand/superhero-girl-medium.gif'; break;
+                    case 'boy': scope.image = '/modules/core/img/brand/superhero-boy-medium.gif'; break;
+                    case 'man': scope.image = '/modules/core/img/brand/teacher-man-medium.gif'; break;
+                    case 'guru': scope.image = '/modules/core/img/brand/guru-medium.gif'; break;
+                    case 'philosopher': scope.image = '/modules/core/img/brand/philosopher-medium.gif'; break;
+                }
             },
             templateUrl: '/modules/core/views/templates/infobox.html'
         };

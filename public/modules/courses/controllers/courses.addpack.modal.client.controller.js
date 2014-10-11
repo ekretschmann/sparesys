52c2c5da6416c1d70700a43e3f7b57af1c55e086
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2014-10-11 07:39:52 +0100

    starting to improve courses

--- public/modules/courses/controllers/courses.addpack.modal.client.controller.js
+++ public/modules/courses/controllers/courses.addpack.modal.client.controller.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module('courses').controller('AddPackToCourseController', ['$scope', '$state', '$timeout', '$modalInstance', 'course', 'Packs', 'Courses','JourneyService',
-	function($scope, $state, $timeout, $modalInstance, course, Packs, Courses, JourneyService) {
+angular.module('courses').controller('AddPackToCourseController', ['$scope', '$state', '$timeout', '$modalInstance', 'course', 'Packs', 'Courses', 'JourneyService',
+    function ($scope, $state, $timeout, $modalInstance, course, Packs, Courses, JourneyService) {
         $scope.course = course;
+        $scope.data = {};
+        $scope.data.name = '';
 
-        $scope.setFocus = function () {
-            $timeout(function(){
-                angular.element('.focus').trigger('focus');
-            },100);
-        };
+        $timeout(function () {
+            angular.element('.focus').trigger('focus');
+        }, 100);
 
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
@@ -24,7 +24,7 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
 
             // Create new Pack object
             var pack = new Packs({
-                name: this.name,
+                name: $scope.data.name,
                 course: this.course._id
             });
 
@@ -33,21 +33,22 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
                 var packid = response._id;
                 $scope.course.packs.push(packid);
                 $scope.course.$update(function () {
-                    $scope.name = '';
-                    $state.go($state.$current, null, { reload: true });
+                    $scope.data.name = '';
+                    angular.element('.focus').trigger('focus');
+//                    $state.go($state.$current, null, { reload: true });
                 }, function (errorResponse) {
                     $scope.error = errorResponse.data.message;
                 });
 
 
-                $scope.course.slaves.forEach(function(slaveId) {
+                $scope.course.slaves.forEach(function (slaveId) {
                     Courses.query({
                         _id: slaveId
                     }, function (slaveCourses) {
 
                         if (slaveCourses.length === 0) {
                             // course does not exist any more
-                            self.slavesSaved ++;
+                            self.slavesSaved++;
                             for (var i in $scope.course.slaves) {
                                 if ($scope.course.slaves[i] === slaveId) {
                                     $scope.course.slaves.splice(i, 1);
@@ -63,17 +64,17 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
                                 course: slaveCourse._id
                             });
 
-                            slavePack.$save(function() {
+                            slavePack.$save(function () {
                                 slaveCourse.packs.push(slavePack._id);
                                 self.slaves.push(slavePack._id);
-                                self.slavesSaved ++;
+                                self.slavesSaved++;
                                 if (self.slavesSaved === self.slavesToSave) {
                                     pack.slaves = self.slaves;
-                                    pack.$update(function(err) {
+                                    pack.$update(function (err) {
 //                                        console.log('updated pack '+pack);
 //                                        console.log(err);
                                     });
-                                    slaveCourse.$update(function(err) {
+                                    slaveCourse.$update(function (err) {
 //                                        console.log('updated course '+slaveCourse);
 //                                        console.log(err);
                                     });
@@ -91,9 +92,7 @@ angular.module('courses').controller('AddPackToCourseController', ['$scope', '$s
                 $scope.error = errorResponse.data.message;
             });
 
-            JourneyService.packCreated();
-            this.name = '';
-            angular.element('.focus').trigger('focus');
+
         };
-	}
+    }
 ]);

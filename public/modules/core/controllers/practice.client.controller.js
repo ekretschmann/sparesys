8fd9a8202d685001b8e572dc9ead0517d075e2b6
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-02-16 17:47:10 +0000

    starting with reverse my answer counts

--- public/modules/core/controllers/practice.client.controller.js
+++ public/modules/core/controllers/practice.client.controller.js
@@ -192,6 +192,9 @@ angular.module('core').controller('PracticeController', ['$scope', '$state', '$m
 
             $scope.card = bestCard;
             $scope.mode = bestCard.modes[Math.floor(Math.random() * bestCard.modes.length)];
+
+            $scope.mode = 'reverse';
+
             $scope.assess = 'self';
             if ($scope.card.hrt && $scope.card.hrt > 1000*60*60*24*5) {
                 $scope.assess = 'auto';

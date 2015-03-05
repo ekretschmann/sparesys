77611b1a813c65b13933bd20746d19e35010d6a6
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-03-05 08:18:36 +0000

    getting behaviour into modal

--- public/modules/schoolclasses/controllers/schoolclassess.setup.modal.client.controller.js
+++ public/modules/schoolclasses/controllers/schoolclassess.setup.modal.client.controller.js
@@ -11,18 +11,6 @@ angular.module('schoolclasses').controller('SetupClassController', ['$scope', '$
 
 
 
-            //for (var i in $scope.school.schoolclasses) {
-            //    if ($scope.school.schoolclasses[i] === $scope.schoolclass) {
-            //        $scope.school.schoolclasses.splice(i, 1);
-            //    }
-            //}
-            //
-            //new Schools($scope.school).$update(function() {
-            //    new Schoolclasses($scope.schoolclass).$remove(function () {
-            //        $state.go($state.$current, null, {reload: true});
-            //    });
-            //});
-
 
             $modalInstance.close();
 

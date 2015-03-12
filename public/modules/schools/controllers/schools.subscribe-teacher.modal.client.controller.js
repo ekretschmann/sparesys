d71ad0dbb3e99c6584fdf7cc6574faa81e00ee13
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-03-12 08:17:39 +0000

    subsribing and unsubscribing as teacher now with GA

--- public/modules/schools/controllers/schools.subscribe-teacher.modal.client.controller.js
+++ public/modules/schools/controllers/schools.subscribe-teacher.modal.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('schools').controller('SubscribeTeacherModalController', ['$scope', '$location', '$modalInstance', 'Authentication', 'school',
-    function ($scope, $location, $modalInstance, Authentication, school) {
+angular.module('schools').controller('SubscribeTeacherModalController', ['$scope', '$window', '$location', '$modalInstance', 'Authentication', 'school',
+    function ($scope, $window, $location, $modalInstance, Authentication, school) {
 
         $scope.authentication = Authentication;
         $scope.school = school;
@@ -9,6 +9,14 @@ angular.module('schools').controller('SubscribeTeacherModalController', ['$scope
         $scope.ok = function () {
             school.teachers.push($scope.authentication.user._id);
             school.$update();
+
+            console.log('ga teacher subscribes to school');
+            console.log('/schools/subscribe/teacher/:id');
+            if ($window.ga) {
+                console.log('sending to ga');
+                $window.ga('send', 'pageview', '/schools/subscribe/teacher/:id');
+                $window.ga('send', 'event', 'user subscribes to a school as teacher');
+            }
             $modalInstance.close();
         };
 
--- public/modules/schools/controllers/schools.unsubscribe-teacher.modal.client.controller.js
+++ public/modules/schools/controllers/schools.unsubscribe-teacher.modal.client.controller.js
@@ -1,13 +1,21 @@
 'use strict';
 
-angular.module('schools').controller('UnsubscribeTeacherModalController', ['$scope', '$location', '$modalInstance', 'Authentication', 'school',
-    function ($scope, $location, $modalInstance, Authentication, school) {
+angular.module('schools').controller('UnsubscribeTeacherModalController', ['$scope', '$location', '$window','$modalInstance', 'Authentication', 'school',
+    function ($scope, $location, $window, $modalInstance, Authentication, school) {
 
         $scope.authentication = Authentication;
         $scope.school = school;
 
         $scope.ok = function () {
 
+
+            console.log('ga teacher unsubscribes from school');
+            console.log('/schools/unsubscribe/teacher/:id');
+            if ($window.ga) {
+                console.log('sending to ga');
+                $window.ga('send', 'pageview', '/schools/unsubscribe/teacher/:id');
+                $window.ga('send', 'event', 'user unsubscribes from a school as teacher');
+            }
 
             for (var i in school.teachers) {
                 if (school.teachers[i] === $scope.authentication.user._id) {

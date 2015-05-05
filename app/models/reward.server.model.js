e2ee603686cffd79897cce4c564cfe8708923f74
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-05-05 12:37:32 +0100

    can save rewards in mongo

--- app/models/reward.server.model.js
+++ app/models/reward.server.model.js
@@ -4,29 +4,38 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Reward Schema
  */
 var RewardSchema = new Schema({
-	name: {
-		type: String,
-		default: '',
-		required: 'Please fill Reward name',
-		trim: true
-	},
-	ingredients: {
-
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+    name: {
+        type: String,
+        default: '',
+        required: 'Please fill Reward name',
+        trim: true
+    },
+    ingredients: [{
+        name: {
+            type: String,
+            default: '',
+            required: 'Please fill Reward name',
+            trim: true
+        },
+        amount: {
+            type: Number,
+            default: 1
+        }
+    }],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
 });
 
 mongoose.model('Reward', RewardSchema);
--- public/modules/rewards/controllers/rewards.client.controller.js
+++ public/modules/rewards/controllers/rewards.client.controller.js
@@ -5,12 +5,31 @@ angular.module('rewards').controller('RewardsController', ['$scope', '$statePara
 	function($scope, $stateParams, $location, Authentication, Rewards) {
 		$scope.authentication = Authentication;
 
+		$scope.ingredients = [];
+
+		$scope.addIngredient = function(reward) {
+
+			var found = false;
+			$scope.ingredients.forEach(function(ingredient) {
+				if (ingredient.name === reward.name) {
+					ingredient.amount += 1;
+					found = true;
+				}
+			});
+
+			if (!found) {
+				$scope.ingredients.push({name: reward.name, amount: 1});
+			}
+		};
+
 		// Create new Reward
 		$scope.create = function() {
 			// Create new Reward object
 			var reward = new Rewards ({
 				name: this.name
 			});
+
+			reward.ingredients = $scope.ingredients;
 
 			$scope.rewards.push(reward);
 			// Redirect after save
@@ -24,44 +43,44 @@ angular.module('rewards').controller('RewardsController', ['$scope', '$statePara
 			});
 		};
 
-		// Remove existing Reward
-		$scope.remove = function(reward) {
-			if ( reward ) { 
-				reward.$remove();
-
-				for (var i in $scope.rewards) {
-					if ($scope.rewards [i] === reward) {
-						$scope.rewards.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.reward.$remove(function() {
-					$location.path('rewards');
-				});
-			}
-		};
-
-		// Update existing Reward
-		$scope.update = function() {
-			var reward = $scope.reward;
-
-			reward.$update(function() {
-				$location.path('rewards/' + reward._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-
+		//// Remove existing Reward
+		//$scope.remove = function(reward) {
+		//	if ( reward ) {
+		//		reward.$remove();
+        //
+		//		for (var i in $scope.rewards) {
+		//			if ($scope.rewards [i] === reward) {
+		//				$scope.rewards.splice(i, 1);
+		//			}
+		//		}
+		//	} else {
+		//		$scope.reward.$remove(function() {
+		//			$location.path('rewards');
+		//		});
+		//	}
+		//};
+        //
+		//// Update existing Reward
+		//$scope.update = function() {
+		//	var reward = $scope.reward;
+        //
+		//	reward.$update(function() {
+		//		$location.path('rewards/' + reward._id);
+		//	}, function(errorResponse) {
+		//		$scope.error = errorResponse.data.message;
+		//	});
+		//};
+        //
 		// Find a list of Rewards
 		$scope.find = function() {
 			$scope.rewards = Rewards.query();
 		};
-
-		// Find existing Reward
-		$scope.findOne = function() {
-			$scope.reward = Rewards.get({ 
-				rewardId: $stateParams.rewardId
-			});
-		};
+        //
+		//// Find existing Reward
+		//$scope.findOne = function() {
+		//	$scope.reward = Rewards.get({
+		//		rewardId: $stateParams.rewardId
+		//	});
+		//};
 	}
 ]);

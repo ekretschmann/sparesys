2d52f1ef0c3b9a92709805425adebca6408b81e1
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-06-04 07:14:39 +0100

    more diagrams

--- public/modules/core/services/diagrams.client.service.js
+++ public/modules/core/services/diagrams.client.service.js
@@ -15,6 +15,109 @@ angular.module('core').service('DiagramsService', [
 
         var format = d3.time.format('%Y-%m-%d');
 
+
+        this.drawLineChart = function (cards, id) {
+
+            var d = [];
+            var earliestDate = Date.now();
+
+
+            for (var i = 0; i < cards.length; i++) {
+                var card = cards[i];
+                if (card.history.length === 0) {
+                    continue;
+                }
+                var key = this.getDateKey(card.history[0].when);
+                if(card.history[0].when < earliestDate) {
+                    earliestDate = card.history[0].when;
+                }
+                if (d[key]) {
+                    d[key] += 1;
+                } else {
+                    d[key] = 1;
+                }
+            }
+
+
+            d[this.getDateKey(earliestDate - 1000*60*60*24)] = 0;
+
+            //var parseDate = d3.time.format('%Y-%m-%d').parse;
+
+            var data = [];
+            Object.keys(d).forEach(function(key) {
+                var point = {};
+
+                point.date = d3.time.format('%Y-%m-%d').parse(key);
+
+                point.close = d[key];
+                data.push(point);
+
+            }, this);
+
+            var margin = {top: 20, right: 20, bottom: 30, left: 50},
+                width = 680 - margin.left - margin.right,
+                height = 300 - margin.top - margin.bottom;
+
+
+            var x = d3.time.scale()
+                .range([0, width]);
+
+            var y = d3.scale.linear()
+                .range([height, 0]);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient('bottom');
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient('left');
+
+            var line = d3.svg.line()
+                .x(function(d) { return x(d.date); })
+                .y(function(d) { return y(d.close); });
+
+            var svg = d3.select(id).append('svg')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+                .attr('background-color', '#FF0000')
+                .append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+
+
+                x.domain(d3.extent(data, function (d) {
+                    return d.date;
+                }));
+                y.domain(d3.extent(data, function (d) {
+                    return d.close;
+                }));
+
+                svg.append('g')
+                    .attr('class', 'x axis')
+                    .attr('transform', 'translate(0,' + height + ')')
+                    .call(xAxis);
+
+                svg.append('g')
+                    .attr('class', 'y axis')
+                    .call(yAxis)
+                    .append('text')
+                    .attr('transform', 'rotate(-90)')
+                    .attr('y', 6)
+                    .attr('dy', '.71em')
+                    .style('text-anchor', 'end')
+                    .text('Cards');
+
+                svg.append('path')
+                    .datum(data)
+                    .attr('class', 'line')
+                    .attr('d', line);
+
+
+        };
+
+
+
         this.liquidFillGaugeDefaultSettings = function () {
             return {
                 minValue: 0, // The gauge minimum value.
@@ -228,192 +331,6 @@ angular.module('core').service('DiagramsService', [
             }
 
 
-        };
-
-        this.drawLineChart = function (cards, id) {
-
-            var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                width = 960 - margin.left - margin.right,
-                height = 500 - margin.top - margin.bottom;
-
-            var parseDate = d3.time.format('%d-%b-%y').parse;
-
-            var x = d3.time.scale()
-                .range([0, width]);
-
-            var y = d3.scale.linear()
-                .range([height, 0]);
-
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient('bottom');
-
-            var yAxis = d3.svg.axis()
-                .scale(y)
-                .orient('left');
-
-            var line = d3.svg.line()
-                .x(function(d) { return x(d.date); })
-                .y(function(d) { return y(d.close); });
-
-            console.log(id);
-            var svg = d3.select(id).append('svg')
-                .attr('width', width + margin.left + margin.right)
-                .attr('height', height + margin.top + margin.bottom)
-                .attr('background-color', '#FF0000')
-                .append('g')
-                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-            d3.tsv('/modules/core/data/data.tsv', function(error, data) {
-                data.forEach(function (d) {
-
-                    d.date = parseDate(d.date);
-                    d.close = +d.close;
-                });
-
-                x.domain(d3.extent(data, function (d) {
-                    return d.date;
-                }));
-                y.domain(d3.extent(data, function (d) {
-                    return d.close;
-                }));
-
-                svg.append('g')
-                    .attr('class', 'x axis')
-                    .attr('transform', 'translate(0,' + height + ')')
-                    .call(xAxis);
-
-                svg.append('g')
-                    .attr('class', 'y axis')
-                    .call(yAxis)
-                    .append('text')
-                    .attr('transform', 'rotate(-90)')
-                    .attr('y', 6)
-                    .attr('dy', '.71em')
-                    .style('text-anchor', 'end')
-                    .text('Price ($)');
-
-                svg.append('path')
-                    .datum(data)
-                    .attr('class', 'line')
-                    .attr('d', line);
-            });
-
-        };
-
-        this.drawLineChartx = function (cards, id) {
-
-            var data = [];
-
-            for (var i = 0; i < cards.length; i++) {
-                var card = cards[i];
-                if (card.history.length === 0) {
-                    continue;
-                }
-                var key = this.getDateKey(card.history[0].when);
-                if (data[key]) {
-                    data[key] += 1;
-                } else {
-                    data[key] = 1;
-                }
-
-            }
-
-            var d = [];
-
-            var parseDate = d3.time.format('%Y-%m-%d').parse;
-
-
-            Object.keys(data).forEach(function(key) {
-                d.date = parseDate(key);
-                d.close = data[key];
-            });
-            //console.log(data);
-            //data.forEach(function (d) {
-            //    //console.log(d);
-            //    d.date = parseDate(d.date);
-            //    d.close = +d.close;
-            //});
-
-            console.log(d);
-
-            var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                width = 960 - margin.left - margin.right,
-                height = 500 - margin.top - margin.bottom;
-
-
-            var x = d3.time.scale()
-                .range([0, width]);
-
-            var y = d3.scale.linear()
-                .range([height, 0]);
-
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient('bottom');
-
-            var yAxis = d3.svg.axis()
-                .scale(y)
-                .orient('left');
-
-            var line = d3.svg.line()
-                .x(function (d) {
-                    return x(d.date);
-                })
-                .y(function (d) {
-                    return y(d.close);
-                });
-
-            var svg = d3.select(id)
-                .attr('width', width + margin.left + margin.right)
-                .attr('height', height + margin.top + margin.bottom)
-                .append('g')
-                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-            //d3.tsv('data.tsv', function (error, data) {
-            //    data.forEach(function (d) {
-            //        d.date = parseDate(d.date);
-            //        d.close = +d.close;
-            //    });
-            //
-            //    x.domain(d3.extent(data, function (d) {
-            //        return d.date;
-            //    }));
-            //    y.domain(d3.extent(data, function (d) {
-            //        return d.close;
-            //    }));
-
-
-            console.log('aaaaa');
-
-            //
-            //x.domain(d3.extent(d, function (d) {
-            //    return d.date;
-            //}));
-            //y.domain(d3.extent(d, function (d) {
-            //    return d.close;
-            //}));
-
-            svg.append('g')
-                .attr('class', 'x axis')
-                .attr('transform', 'translate(0,' + height + ')')
-                .call(xAxis);
-
-            svg.append('g')
-                .attr('class', 'y axis')
-                .call(yAxis)
-                .append('text')
-                .attr('transform', 'rotate(-90)')
-                .attr('y', 6)
-                .attr('dy', '.71em')
-                .style('text-anchor', 'end')
-                .text('Price ($)');
-
-            svg.append('path')
-                .datum(d)
-                .attr('class', 'line')
-                .attr('d', line);
-            //});
         };
 
 

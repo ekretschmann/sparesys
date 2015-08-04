28dda558910d4be1545b18dc101449e4ef94a74a
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-08-04 08:01:18 +0100

    moved gauge code into separate service

--- public/modules/core/services/diagrams-gauge.client.service.js
+++ public/modules/core/services/diagrams-gauge.client.service.js
@@ -1,30 +1,10 @@
 'use strict';
 
 /* global d3 */
-angular.module('core').service('DiagramsGaugeService', [
-    function () {
+angular.module('core').service('DiagramsGaugeService', ['$timeout',
+    function ($timeout) {
 
 
-
-        this.loadLiquidFillGauge = function (score, colorCode) {
-
-            if (score < 0) {
-                return;
-            }
-
-            $timeout(function () {
-                var config1 = DiagramsService.liquidFillGaugeDefaultSettings();
-                config1.circleColor = colorCode;
-                config1.textColor = colorCode;
-                config1.waveTextColor = colorCode;
-                config1.waveColor = '#CCCCFF';
-                config1.circleThickness = 0.2;
-                config1.textVertPosition = 0.2;
-                config1.waveAnimateTime = 1000;
-                this.drawLiquidFillGauge('fillgauge', score, config1);
-            }, 200);
-
-        };
 
 
         this.liquidFillGaugeDefaultSettings = function () {
@@ -54,12 +34,6 @@ angular.module('core').service('DiagramsGaugeService', [
 
         this.drawLiquidFillGauge = function (elementId, value, config) {
 
-
-
-            console.log('xxxxx');
-            console.log(elementId);
-            console.log(value);
-            console.log(config);
 
             if (config === null) config = this.liquidFillGaugeDefaultSettings();
 
@@ -258,6 +232,25 @@ angular.module('core').service('DiagramsGaugeService', [
         };
 
 
+        this.loadLiquidFillGauge = function (score, colorCode) {
+
+            if (score < 0) {
+                return;
+            }
+
+            //$timeout(function () {
+                var config1 = this.liquidFillGaugeDefaultSettings();
+                config1.circleColor = colorCode;
+                config1.textColor = colorCode;
+                config1.waveTextColor = colorCode;
+                config1.waveColor = '#CCCCFF';
+                config1.circleThickness = 0.2;
+                config1.textVertPosition = 0.2;
+                config1.waveAnimateTime = 1000;
+                this.drawLiquidFillGauge('fillgauge', score, config1);
+            //}, 200);
+
+        };
 
     }
 ])

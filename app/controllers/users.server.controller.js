eec0b4058ca6592b44329331d3572454703facd2
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-08-12 16:34:46 +0100

    finding users improved

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -517,18 +517,36 @@ exports.list = function (req, res) {
 
     console.log(req.query.text);
     if (req.query && req.query.text) {
-        var search = req.query.text;
-        User.find( { $or: [{'firstName': {$regex: '^'+search}}, {'lastName': {$regex: '^'+search}}]}).limit(3).exec(function(err, users){
-            //console.log(err);
-            //console.log(users);
-            if (err) {
-                return res.send(400, {
-                    message: getErrorMessage(err)
-                });
-            } else {
-                res.jsonp(users);
-            }
-        });
+        var search = req.query.text.split(' ');
+
+        if (search.length === 1) {
+            User.find({$or: [{'firstName': {$regex: '^' + search[0]}}, {'lastName': {$regex: '^' + search[0]}}]}).limit(25).exec(function (err, users) {
+                //console.log(err);
+                //console.log(users);
+                if (err) {
+                    return res.send(400, {
+                        message: getErrorMessage(err)
+                    });
+                } else {
+                    res.jsonp(users);
+                }
+            });
+        } else if (search.length === 2) {
+            console.log(search);
+            User.find({$and: [{'firstName': {$regex: '^' + search[0]}}, {'lastName': {$regex: '^' + search[1]}}]}).limit(25).exec(function (err, users) {
+                //console.log(err);
+                //console.log(users);
+                if (err) {
+                    return res.send(400, {
+                        message: getErrorMessage(err)
+                    });
+                } else {
+                    res.jsonp(users);
+                }
+            });
+        } else {
+            res.jsonp([]);
+        }
     } else {
 
         User.find({}, '-salt -password -__v -provider').sort('-created').populate('_id').exec(function (err, users) {

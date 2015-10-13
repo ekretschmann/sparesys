b257279ccdc83365e5f0d598c2842c6380e10b13
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-10-13 09:58:26 +0100

    can now subscribe as teacher

--- public/modules/schools/controllers/schools.subscribe-teacher.modal.client.controller.js
+++ public/modules/schools/controllers/schools.subscribe-teacher.modal.client.controller.js
@@ -10,29 +10,17 @@ angular.module('schools').controller('SubscribeTeacherModalController', ['$scope
         $scope.ok = function () {
 
 
-            console.log(user);
             Users.get({
                 userId: user._id
             }, function (result) {
 
                 result.teacherInSchools.push($scope.school._id);
-                result.$update(function() {
+                result.$update(function () {
                     $scope.user.teacherInSchools.push($scope.school._id);
                     $modalInstance.close();
                 });
             });
 
-
-
-            //
-            //
-            //console.log('ga teacher subscribes to school');
-            //console.log('/schools/subscribe/teacher/:id');
-            //if ($window.ga) {
-            //    console.log('sending to ga');
-            //    $window.ga('send', 'pageview', '/schools/subscribe/teacher/:id');
-            //    $window.ga('send', 'event', 'user subscribes to a school as teacher');
-            //}
 
         };
 

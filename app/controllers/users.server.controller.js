de22e1c172bd03de2a2a35c8cd35a09ba457fcda
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-10-18 08:56:38 +0100

    revert to normal

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -123,37 +123,37 @@ exports.signin = function (req, res, next) {
     //console.log(req.body);
 
 
-    User.findById('540b21baf4940c0200338a5d', function (err, user) {
-
-        //console.log(user);
-        req.login(user, function (err) {
-                        if (err) {
-                            res.send(400, err);
-                        } else {
-                            res.jsonp(user);
-                        }
-                    });
-
-    });
-
-    //passport.authenticate('local', function (err, user, info) {
+    //User.findById('540b21baf4940c0200338a5d', function (err, user) {
     //
-    //    if (err || !user) {
-    //        res.send(400, info);
-    //    } else {
-    //        // Remove sensitive data before login
-    //        user.password = undefined;
-    //        user.salt = undefined;
+    //    //console.log(user);
+    //    req.login(user, function (err) {
+    //                    if (err) {
+    //                        res.send(400, err);
+    //                    } else {
+    //                        res.jsonp(user);
+    //                    }
+    //                });
     //
-    //        req.login(user, function (err) {
-    //            if (err) {
-    //                res.send(400, err);
-    //            } else {
-    //                res.jsonp(user);
-    //            }
-    //        });
-    //    }
-    //})(req, res, next);
+    //});
+
+    passport.authenticate('local', function (err, user, info) {
+
+        if (err || !user) {
+            res.send(400, info);
+        } else {
+            // Remove sensitive data before login
+            user.password = undefined;
+            user.salt = undefined;
+
+            req.login(user, function (err) {
+                if (err) {
+                    res.send(400, err);
+                } else {
+                    res.jsonp(user);
+                }
+            });
+        }
+    })(req, res, next);
 };
 
 var updateSchoolForTeacher = function (theUser, req) {

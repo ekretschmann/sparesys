2bdd1bdeba44f56ba3f9feba8e48358ed79844e8
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2015-10-19 20:20:05 +0100

    small ux improvements

--- public/modules/courses/directives/upload.client.directive.js
+++ public/modules/courses/directives/upload.client.directive.js
@@ -7,24 +7,26 @@ angular.module('courses').directive('upload',
                 file:'='
             },
             link: function(scope, element) {
-                //$(element).on('change', function(changeEvent) {
-                //    console.log('here');
-                //    console.log(changeEvent.target.files);
-                //    var files = changeEvent.target.files;
-                //    if (files.length) {
-                //        var r = new FileReader();
-                //        r.onload = function(e) {
-                //            var contents = e.target.result;
-                //            //scope.$apply(function () {
-                //            //    scope.fileReader = contents;
-                //            //});
-                //
-                //            console.log(contents);
-                //        };
-                //
-                //        r.readAsText(files[0]);
-                //    }
-                //});
+                /* jshint ignore:start */
+                $(element).on('change', function(changeEvent) {
+                    console.log('here');
+                    console.log(changeEvent.target.files);
+                    var files = changeEvent.target.files;
+                    if (files.length) {
+                        var r = new FileReader();
+                        r.onload = function(e) {
+                            var contents = e.target.result;
+                            //scope.$apply(function () {
+                            //    scope.fileReader = contents;
+                            //});
+
+                            console.log(contents);
+                        };
+
+                        r.readAsText(files[0]);
+                    }
+                });
+                /* jshint ignore:end */
             }
         };
     }

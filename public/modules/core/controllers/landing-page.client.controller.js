f1d92b66b1b9c40ab887e4873a8f2d23a9003850
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2016-01-27 07:48:03 +0000

    before css refactoring

--- public/modules/core/controllers/landing-page.client.controller.js
+++ public/modules/core/controllers/landing-page.client.controller.js
@@ -20,12 +20,13 @@ angular.module('core').controller('LandingPageController', ['$window', '$scope',
 
         // Set of Photos
         $scope.slides = [
-            {image: '/modules/core/img/brand/superhero-girl-medium.gif', text: 'You won\'t forget this. Ever.'},
+            {image: '/modules/core/img/brand/gator-skater.png', text: 'You won\'t forget this. Ever.'},
             {image: '/modules/core/img/brand/superhero-boy-medium.gif', text: 'I dare you to become a Rememberator!'},
             {image: '/modules/core/img/brand/philosopher-medium.gif', text: 'I remember, therefore I am.'},
             {image: '/modules/core/img/brand/guru-medium.gif', text: 'Unleash your brainpower!'},
             {image: '/modules/core/img/brand/teacher-man-medium.gif', text: 'Do something worth Remembering!'},
             {image: '/modules/core/img/brand/teacher-woman-medium.gif', text: 'Forget about Forgetting!'}
+
         ];
 
 

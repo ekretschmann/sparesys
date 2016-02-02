a825b5a70ace40c4bd2214e3abe440684d0c7808
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2016-02-02 05:57:47 +0000

    edit card only half finished but can't bear it no longer

--- public/modules/core/directives/infobox.client.directive.js
+++ public/modules/core/directives/infobox.client.directive.js
@@ -16,7 +16,7 @@ angular.module('core').directive('infobox',
                 switch (scope.logo) {
                     case 'woman': scope.image = '/modules/core/img/brand/bear.png'; break;
                     case 'girl': scope.image = '/modules/core/img/brand/superhero-girl-medium.gif'; break;
-                    case 'boy': scope.image = '/modules/core/img/brand/superhero-boy-medium.gif'; break;
+                    case 'boy': scope.image = '/modules/core/img/brand/illustrator.png'; break;
                     case 'man': scope.image = '/modules/core/img/brand/fish.png'; break;
                     case 'guru': scope.image = '/modules/core/img/brand/gator-skater.png'; break;
                     case 'philosopher': scope.image = '/modules/core/img/brand/philosopher-medium.gif'; break;
